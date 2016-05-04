4d7a0d32de935f4e8141f6f62b6bd45109e11893
Chandler De Angelis <chandler.dea@me.com>
2016-05-04 13:05:14 -0700

    Added Carthage support

---
+++ JSQMessagesViewController/JSQMessagesViewControllerPublic.h
@@ -0,0 +1,21 @@
+//
+//  JSQMessagesViewController.h
+//  JSQMessagesViewController
+//
+//  Created by Chandler De Angelis on 5/4/16.
+//  Copyright © 2016 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSQMessagesViewController.
+FOUNDATION_EXPORT double JSQMessagesViewControllerVersionNumber;
+
+//! Project version string for JSQMessagesViewController.
+FOUNDATION_EXPORT const unsigned char JSQMessagesViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesViewController/PublicHeader.h>
+#import "JSQMessages.h"
+#import "JSQMessagesViewController.h"
+
+
