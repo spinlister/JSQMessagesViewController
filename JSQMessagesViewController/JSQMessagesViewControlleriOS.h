73a01e0a2de7b7adbe75a5ce379a1174f24eb909
Chandler De Angelis <chandler.dea@me.com>
2016-05-04 14:45:28 -0700

    Making JSQMessage.h file public

--- JSQMessagesViewController/JSQMessagesViewControlleriOS.h
+++ JSQMessagesViewController/JSQMessagesViewControlleriOS.h
@@ -16,6 +16,5 @@ FOUNDATION_EXPORT const unsigned char JSQMessagesViewControllerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesViewController/PublicHeader.h>
 #import "JSQMessages.h"
-#import "JSQMessagesViewController.h"
 
 
